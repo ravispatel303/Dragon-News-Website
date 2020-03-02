6b7464555711180e91ee06cef3eb4ae700187e99
Ravi Patel <ravispatel303@gmail.com>
2020-03-02 16:07:50 -0500

    Files added!!!

---
+++ DUI/a1/run.py
@@ -0,0 +1,30 @@
+#Ravi Patel, rp837@drexel.edu
+#CS530: DUI Project
+
+from flask import Flask, render_template, send_file
+import os
+
+
+app = Flask(__name__, static_folder='public', static_url_path='')
+
+
+# Handle the index (home) page
+@app.route('/')
+def index():
+    return render_template('index.html')
+   
+
+# Handle any files that begin "/course" by loading from the course directory
+@app.route('/course/<path:path>')
+def base_static(path):
+    return send_file(os.path.join(app.root_path, '..', 'course', path))
+
+
+# Handle any unhandled filename by loading its template
+@app.route('/<name>')
+def generic(name):
+    return render_template(name + '.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8102, debug=True)
